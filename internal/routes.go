3f4179e920e5801c26b6ca6ead62454066acb9ab
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-14 23:37:37 +0300

    added abstract

--- internal/routes.go
+++ internal/routes.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
 
@@ -9,6 +8,6 @@ import (
 )
 
 // Not Implemented
-func addRoutes(mux *http.ServeMux, logger *slog.Logger, config *test_medods.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) { fmt.Println("Not Implemented") }
+func addRoutes(mux *http.ServeMux, logger *slog.Logger, config *test_medods.Config) {
+	// Not Implemented
 }
