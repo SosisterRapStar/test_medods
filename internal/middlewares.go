052a2eb60a3015edb791d23204deeb57508d23e4
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-14 23:36:47 +0300

    added abstract

--- internal/middlewares.go
+++ internal/middlewares.go
@@ -1,3 +1,12 @@
 package internal
 
+import "net/http"
+
 // Not implemented yet
+func somemiddleware(h http.HandlerFunc) http.HandlerFunc {
+	// do something
+	return func(rw http.ResponseWriter, r *http.Request) {
+		// not implemented
+		h(rw, r)
+	}
+}
