d44b21ebc20a8e6b8f92d364284b12895156fafb
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-16 00:55:07 +0300

    added realization

--- internal/auth/auth_service.go
+++ internal/auth/auth_service.go
@@ -17,6 +17,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const DEFAULT_INTERNAL_ERROR_STRING = "internal server error"
+
 func startTransaction(ctx context.Context, conn *pgxpool.Conn, isolation pgx.TxIsoLevel) (pgx.Tx, error) {
 	tx, err := conn.BeginTx(ctx, pgx.TxOptions{IsoLevel: isolation})
 	if err != nil {
@@ -36,8 +38,37 @@ type AuthService struct {
 	*postgres.PostgresConnection
 }
 
-func (a *AuthService) parseToken(token string) {
+func (a *AuthService) checkIfUserLoggedOut(ctx context.Context, userId string, issuedAt *jwt.NumericDate) (bool, error) {
+	// not implemented
+	return nil, nil
+}
 
+func (a *AuthService) AuthenticateUser(tokenString string) (string, error) {
+	token, err := a.validateToken(tokenString, a.c.Auth.SecretKey)
+	if err != nil {
+		return "", err
+	}
+	claims := token.Claims
+	userId, err := claims.GetSubject()
+	if err != nil {
+		return "", &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
+	}
+
+	return userId, nil
+}
+
+func (a *AuthService) validateToken(tokenString string, key string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			a.logger.Warn(fmt.Sprintf("Strange activity token can be changed on client side %s"))
+			return nil, errors.New("unexpected sign method token invalid")
+		}
+		return []byte(key), nil
+	})
+	if err != nil {
+		return nil, &core.AuthorizationError{Err: errors.New("can not authorize user")}
+	}
+	return token, err
 }
 
 func (a *AuthService) generateJWT(userId string, expirePeriodMinutes int, secretKey string) (string, error) {
@@ -65,7 +96,7 @@ func (a *AuthService) saveRefreshToken(ctx context.Context, refresh string, user
 	conn, err := a.Pool.Acquire(ctx)
 	if err != nil {
 		a.logger.Error("Error occured during db connection acquiring token creating")
-		return &core.InternalError{Err: errors.New("Server error")}
+		return &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 
 	defer conn.Release()
@@ -73,19 +104,19 @@ func (a *AuthService) saveRefreshToken(ctx context.Context, refresh string, user
 	tx, err := startTransaction(ctx, conn, pgx.ReadCommitted)
 	if err != nil {
 		a.logger.Error("Error occured during starting the transaction in db")
-		return &core.InternalError{Err: errors.New("Server error")}
+		return &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 	defer tx.Rollback(ctx)
 
 	userIdInUUID, err := uuid.Parse(userId)
 	if err != nil {
 		a.logger.Error(fmt.Sprintf("Error occured during parsing user id %s id to uuid", userId))
-		return &core.InternalError{Err: errors.New("Server error")}
+		return &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 	signHash, err := a.getHashFromSign(refresh)
 	if err != nil {
-		a.logger.Error(fmt.Sprintf("Error occured during bcrypt hashing of refresh JWT sign"))
-		return &core.InternalError{Err: errors.New("Server error")}
+		a.logger.Error("Error occured during bcrypt hashing of refresh JWT sign")
+		return &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 	token_info := core.TokenInfo{
 		UserId:     userIdInUUID,
@@ -98,7 +129,7 @@ func (a *AuthService) saveRefreshToken(ctx context.Context, refresh string, user
 	query := "INSERT INTO auth.tokens (token_id, user_id, sign_hash, issued_to_ua, issued_to_ip, is_revoked) VALUES ($1, $2, $3, $4, $5, $6)"
 	if _, err = tx.Exec(ctx, query, token_info.Id, token_info.UserId, token_info.SignHash, token_info.IssuedToUA, token_info.IssuedToIP, token_info.IsRevoked); err != nil {
 		a.logger.Error(fmt.Sprintf("Failed to execute query %s", query))
-		return &core.InternalError{Err: errors.New("Server error")}
+		return &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 	return nil
 
@@ -108,12 +139,12 @@ func (a *AuthService) CreateTokens(ctx context.Context, userId string, userAgent
 	access, err := a.generateJWT(userId, a.c.Auth.AccessTokenExpirePeriodMinutes, a.c.Auth.SecretKey)
 	if err != nil {
 		a.logger.Error("Error occured during access token creating")
-		return nil, &core.InternalError{Err: errors.New("Server error")}
+		return nil, &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 	refresh, err := a.generateJWT(userId, a.c.Auth.AccessTokenExpirePeriodMinutes, a.c.Auth.SecretKey)
 	if err != nil {
 		a.logger.Error("Error occured during refresh token creating")
-		return nil, &core.InternalError{Err: errors.New("Server error")}
+		return nil, &core.InternalError{Err: errors.New(DEFAULT_INTERNAL_ERROR_STRING)}
 	}
 
 	if err := a.saveRefreshToken(ctx, refresh, userId, userAgent, userIp); err != nil {
@@ -124,4 +155,12 @@ func (a *AuthService) CreateTokens(ctx context.Context, userId string, userAgent
 
 // current user
 
-func (a *AuthService) GetCurrentUserGUID(token)
+func (a *AuthService) RevokeAllUserTokens(ctx context.Context, userId string) {
+	// not Implmented
+	return
+}
+
+func (a *AuthService) GetCurrentUserGUID(token string) {
+	// not Implmented
+	return
+}
