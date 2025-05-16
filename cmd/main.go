75f4ea20ac53b704b2554859acca6303c3d32d72
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-17 01:23:03 +0300

    added main

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/sosisterrapstar/test_medods"
+	"github.com/sosisterrapstar/test_medods/internal"
 	"github.com/sosisterrapstar/test_medods/internal/auth"
 	"github.com/sosisterrapstar/test_medods/internal/postgres"
 )
 
-// Abstract
 func main() {
 	config := test_medods.MustLoad()
 	logger := test_medods.SetupLogger()
 	psConn := postgres.PostgresConnection{}
 	psConn.Open(logger, config)
-	authService := &auth.AuthService{
-		logger,
-		config,
-		&psConn,
-	}
+	authService := auth.NewAuthService(logger, config, &psConn)
+	server := internal.NewServer(logger, config, authService)
+	server.Start()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	logger.Info("Received Interrupt signal, started to shutdown gracefully")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	server.Stop(ctx)
 }
