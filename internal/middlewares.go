b67fd6abf40071f8bb7650758e97f3379066e741
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-16 22:34:54 +0300

    refactroed interface

--- internal/middlewares.go
+++ internal/middlewares.go
@@ -1,12 +1,58 @@
 package internal
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/sosisterrapstar/test_medods/internal/core"
+)
+
+type userKey int
+
+const userStructKey = 0
+
+func userFromContext(ctx context.Context) (*core.User, bool) {
+	user, ok := ctx.Value(userStructKey).(*core.User)
+	return user, ok
+
+}
 
 // Not implemented yet
-func somemiddleware(h http.HandlerFunc) http.HandlerFunc {
-	// do something
-	return func(rw http.ResponseWriter, r *http.Request) {
-		// not implemented
-		h(rw, r)
+func authenticationMiddleware(h http.HandlerFunc, auth core.Auth) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			writeJSON(w, http.StatusUnauthorized, map[string]string{"message": "authorization header is required"})
+			return
+		}
+
+		parts := strings.Fields(authHeader)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			writeJSON(w, http.StatusUnauthorized, map[string]string{"message": "invalid authorization header format"})
+			return
+		}
+		token := parts[1]
+
+		if len(token) == 0 {
+			response := map[string]string{"message": "not authorized"}
+			writeJSON(w, http.StatusUnauthorized, response)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		user, err := auth.AuthenticateUser(ctx, token)
+		if err != nil {
+			httpErr := validateError(err)
+			writeJSON(w, httpErr.status, map[string]string{"message": httpErr.message})
+			return
+		}
+		cancel()
+
+		ctx = context.WithValue(context.Background(), userStructKey, user)
+		h(w, r.WithContext(ctx))
 	}
 }
--- internal/routes.go
+++ internal/routes.go
@@ -1,12 +1,42 @@
 package internal
 
 import (
+	"encoding/json"
+	"errors"
 	"log/slog"
 	"net/http"
 
 	"github.com/sosisterrapstar/test_medods"
 	"github.com/sosisterrapstar/test_medods/internal/core"
 )
+
+type HTTPErrorMessage struct {
+	status  int
+	message string
+}
+
+func writeJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, `{"error": "Failed to encode response"}`, http.StatusInternalServerError)
+	}
+}
+
+func validateError(err error) HTTPErrorMessage {
+
+	switch {
+	case errors.As(err, &core.AuthorizationError{}):
+		return HTTPErrorMessage{status: http.StatusUnauthorized, message: err.Error()}
+	case errors.Is(err, &core.ForbiddenError{}):
+		return HTTPErrorMessage{status: http.StatusForbidden, message: err.Error()}
+	case errors.As(err, &core.InternalError{}):
+		return HTTPErrorMessage{status: http.StatusInternalServerError, message: err.Error()} // Исправлено на HTTPErrorMessage
+	case errors.As(err, &core.RequestError{}):
+		return HTTPErrorMessage{status: http.StatusBadRequest, message: err.Error()}
+	default:
+		return HTTPErrorMessage{status: http.StatusInternalServerError, message: "server internal error"}
+	}
+}
 
 // Not Implemented
 func addRoutes(mux *http.ServeMux, logger *slog.Logger, config *test_medods.Config) {
