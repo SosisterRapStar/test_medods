2927984921a5eed28223cdbcb7c6d35203bcc88c
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-16 23:04:26 +0300

    started endpoint

--- internal/routes.go
+++ internal/routes.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/sosisterrapstar/test_medods"
 	"github.com/sosisterrapstar/test_medods/internal/core"
@@ -43,11 +45,25 @@ func addRoutes(mux *http.ServeMux, logger *slog.Logger, config *test_medods.Conf
 	// Not Implemented
 }
 
-// func accessEndpoint(logger *slog.Logger, authService core.Auth) http.HandlerFunc {
-// 	return func(rw http.ResponseWriter, r *http.Request) {
-// 		_ = authService.CreateTokens()
-// 	}
-// }
+func accessEndpoint(logger *slog.Logger, auth core.Auth) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var tokens *core.Tokens
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		id := r.PathValue("id")
+
+		userAgent := r.Header.Get("User-Agent")
+		// сервис не расчитан на работу за балансировщиком, поэтому не проверяет X-Forwarded-For
+		ipAddr := r.RemoteAddr
+
+		tokens, err := auth.CreateTokens(ctx, id, userAgent, ipAddr)
+		if err != nil {
+			httpErr := validateError(err)
+			writeJSON(w, httpErr.status, map[string]string{"message": httpErr.message})
+			return
+		}
+	}
+}
 
 // func refreshTokensEndpoint(logger *slog.Logger, authService core.Auth) http.HandlerFunc {
 // 	return func(rw http.ResponseWriter, r *http.Request) {
@@ -72,7 +88,6 @@ func getCurrentUserGUIDEndpoint(logger *slog.Logger) http.HandlerFunc {
 		}
 		uuid := user.Id.String()
 		writeJSON(w, http.StatusOK, map[string]string{"user_id": uuid})
-		return
 	}
 }
 
