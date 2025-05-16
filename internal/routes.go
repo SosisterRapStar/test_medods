e9e983bd8e35ff0984986312ec3b76c2387e0d6a
SosisterRapStar <timoshenkovvanya@gmail.com>
2025-05-17 00:00:58 +0300

    added endpoint

--- internal/routes.go
+++ internal/routes.go
@@ -27,13 +27,13 @@ func writeJSON(w http.ResponseWriter, status int, data interface{}) {
 func validateError(err error) HTTPErrorMessage {
 
 	switch {
-	case errors.As(err, &core.AuthorizationError{}):
+	case errors.Is(err, &core.AuthorizationError{}):
 		return HTTPErrorMessage{status: http.StatusUnauthorized, message: err.Error()}
 	case errors.Is(err, &core.ForbiddenError{}):
 		return HTTPErrorMessage{status: http.StatusForbidden, message: err.Error()}
-	case errors.As(err, &core.InternalError{}):
+	case errors.Is(err, &core.InternalError{}):
 		return HTTPErrorMessage{status: http.StatusInternalServerError, message: err.Error()} // Исправлено на HTTPErrorMessage
-	case errors.As(err, &core.RequestError{}):
+	case errors.Is(err, &core.RequestError{}):
 		return HTTPErrorMessage{status: http.StatusBadRequest, message: err.Error()}
 	default:
 		return HTTPErrorMessage{status: http.StatusInternalServerError, message: "server internal error"}
@@ -62,20 +62,20 @@ func accessEndpoint(logger *slog.Logger, auth core.Auth) http.HandlerFunc {
 			writeJSON(w, httpErr.status, map[string]string{"message": httpErr.message})
 			return
 		}
+
+		cookie := &http.Cookie{
+			Name:     "refresh_token",
+			Value:    tokens.Refresh,
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		}
+
+		http.SetCookie(w, cookie)
+
+		writeJSON(w, http.StatusOK, map[string]string{"accessToken": tokens.Access})
 	}
 }
-
-// func refreshTokensEndpoint(logger *slog.Logger, authService core.Auth) http.HandlerFunc {
-// 	return func(rw http.ResponseWriter, r *http.Request) {
-// 		_ = authService.UpdateTokens()
-// 	}
-// }
-
-// func updateTokensEndpointRoute(logger *slog.Logger, authService core.Auth) http.HandlerFunc {
-// 	return func(rw http.ResponseWriter, r *http.Request) {
-// 		_ = authService.CreateTokens()
-// 	}
-// }
 
 func getCurrentUserGUIDEndpoint(logger *slog.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -91,8 +91,10 @@ func getCurrentUserGUIDEndpoint(logger *slog.Logger) http.HandlerFunc {
 	}
 }
 
-// func unauthorizeEndpoint(logger *slog.Logger, authService core.Auth) http.HandlerFunc {
-// 	return func(rw http.ResponseWriter, r *http.Request) {
-// 		_ = authService.UnauthorizeUser()
-// 	}
-// }
+func getFromCookies(r *http.Request, key string) string {
+	thingFromCookie, err := r.Cookie(key)
+	if err != nil {
+		return ""
+	}
+	return thingFromCookie.Value
+}
